f40f868a86313b9ce888e444e6d7e4c12cc20f48
olegpetskovich <OlegPetskovich@gmail.com>
2023-01-25 10:23:59 +0200

    EDITED shared_preference.dart

--- lib/utils/shared_preference.dart
+++ lib/utils/shared_preference.dart
@@ -3,14 +3,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SharedPref {
   static const String _loggedStatusKey = 'loggedStatusKey';
 
-  static Future<void> setLoggedInStatus() async {
+  static Future<void> setLoggedStatus(bool isLogged) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    sharedPreferences.setBool(_loggedStatusKey, true);
-  }
-
-  static Future<void> setLoggedOutStatus() async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    sharedPreferences.setBool(_loggedStatusKey, false);
+    sharedPreferences.setBool(_loggedStatusKey, isLogged);
   }
 
   static Future<bool> isLoggedIn() async {
